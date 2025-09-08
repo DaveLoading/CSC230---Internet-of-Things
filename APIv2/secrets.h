20e95fec23d5d0970f504dbeeda1ecb41e33b9c5
DaveLoading <156347281+DaveLoading@users.noreply.github.com>
2025-09-08 13:38:23 -0700

    Delete APIv2/secrets.h

--- APIv2/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "wificlazbloomg"	
-#define SECRET_PASS "656765011316Sham"	
-
-#define DATABASE_URL "https://dsfirebaserecdatav1-default-rtdb.firebaseio.com/" 
-#define API_KEY "AIzaSyAnNdtSLJDdk8ieEMs1lP3En1OXA6VpJdA"  
