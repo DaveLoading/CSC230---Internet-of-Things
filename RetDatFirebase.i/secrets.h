8c49302bd1c61c429a4ef17ff3454a61059dce68
DaveLoading <156347281+DaveLoading@users.noreply.github.com>
2025-09-08 13:37:57 -0700

    Delete RetDatFirebase.i/secrets.h

--- RetDatFirebase.i/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "Johnny Rockets Guest"	
-#define SECRET_PASS "johnnyrockets"	
-
-#define DATABASE_URL "https://dsfirebaserecdatav1-default-rtdb.firebaseio.com/" 
-#define API_KEY "AIzaSyAnNdtSLJDdk8ieEMs1lP3En1OXA6VpJdA"  
