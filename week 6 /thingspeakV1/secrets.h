7ed67aa54a687725f11b313c95bbcc12da8fd7f6
DaveLoading <156347281+DaveLoading@users.noreply.github.com>
2025-09-08 13:38:42 -0700

    Delete week 6 /thingspeakV1/secrets.h

--- week 6 /thingspeakV1/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "Frontierg5552"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "1859115180"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 2997131			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "4SCWIVACG63LYW5N"   // replace XYZ with your channel write API Key
